a0ce8b3a797d003528aeadcaffff059e15aab92a
Muhammad Shahzaib <134364372+hussainkhan0@users.noreply.github.com>
2023-05-28 22:15:11 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-
 import { useState } from 'react';
+import { useRef } from 'react';
 
 function App() {
   const [fruits,setFruits]= useState([])
@@ -9,26 +9,51 @@ function App() {
   const inputUpdate=(event)=>{
     setInputText(event.target.value)
   }
+   const focusOn = useRef();
 
   const updateArr =() => {
+    if(inputText == ""){
+      alert("Not Allowed")
+  }
+  else{
     const newAr=[...fruits,inputText];
     setFruits(newAr);
     setInputText("");
-
+    focusOn.current.focus();
   }
+  }
+
+  const changeArr= (index) => {
+    const result= fruits.toSpliced (index,1) 
+  
+    setFruits(result);
+  }
+
+ const enterKey = (event) => {
+  if(event.key == "Enter"){
+    updateArr();
+  }
+ }
+ 
+  const editArr = (item) => {}
+
+
   return (
     <div className="App">
 
-        <input onChange={inputUpdate} type="text" value={inputText} placeholder="Enter item to list"/>
+        <input onChange={inputUpdate} onKeyUp={enterKey} type="text" value={inputText} ref={focusOn}placeholder="Enter item to list"/>
         <button onClick={updateArr}> click me </button>
+        
        <ul>
         {
           fruits.map((item, index) => {
-            return <li key={index}>{item}</li>
+            return <li key={index}>{item}<button onClick={ () => {changeArr(item)} }>X</button>
+            <button onclick={ () => {editArr(item)}}>edit</button>
+            </li>
           })
         }
        </ul>
-       <h1>hussain khan 123</h1>
+       <h1>Hussain khan</h1>
 
     </div>
   );
